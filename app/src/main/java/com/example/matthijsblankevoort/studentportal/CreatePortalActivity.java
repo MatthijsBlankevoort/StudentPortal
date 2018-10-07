db7e07e8e50d87fdb9ffd09ffb5f2e3d3566354d
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2018-10-07 14:42:22 +0200

    feature: add activities for view portal and create portal

---
+++ app/src/main/java/com/example/matthijsblankevoort/studentportal/CreatePortalActivity.java
@@ -0,0 +1,13 @@
+package com.example.matthijsblankevoort.studentportal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatePortalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_portal);
+    }
+}
